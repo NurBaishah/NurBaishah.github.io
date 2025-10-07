dcf56a0e9f885c7283b32e1cabea7f170292b826
NurBaishah <nurbaishah18@yonsei.ac.kr>
2025-10-08 08:13:38 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -248,29 +248,4 @@
   window.addEventListener('load', navmenuScrollspy);
   document.addEventListener('scroll', navmenuScrollspy);
 
-   const scrollLine = document.querySelector('.scroll-line');
-
-    function updateScrollProgress() {
-      const windowHeight = window.innerHeight;
-      const fullHeight = document.body.clientHeight;
-      const scrolled = window.scrollY;
-      const percentScrolled = (scrolled / (fullHeight - windowHeight)) * 100;
-      scrollLine.style.width = `${percentScrolled}%`;
-    }
-
-    updateScrollProgress();
-
-    let ticking = false;
-    window.addEventListener('scroll', () => {
-      if (!ticking) {
-        window.requestAnimationFrame(() => {
-          updateScrollProgress();
-          ticking = false;
-        });
-        ticking = true;
-      }
-    });
-
-    window.addEventListener('resize', updateScrollProgress);
-
 })();
